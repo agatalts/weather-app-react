9157277bbb83200023216d545570f7f30cc432a0
agatalts <agata.e.latos@gmail.com>
2021-01-10 22:04:02 +0000

    Uploaded little changes to see if GitHub and Netfliy work correctly

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function Weather() {
   function handleSubmit(response) {
-   alert(`Is it ${response.data.main.temp} C`);
+   alert(`It is ${response.data.main.temp} C in London`);
   }
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=791caf474f5e47b0c7a34593ae174a7e&units=metric`;
     axios.get(apiUrl).then(handleSubmit);
