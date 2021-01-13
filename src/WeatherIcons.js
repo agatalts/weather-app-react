154cc0a19e52d4a57a134ae87f736df790f834c4
agatalts <agata.e.latos@gmail.com>
2021-01-13 14:31:10 +0000

    added weather icon change function

--- src/WeatherIcons.js
+++ src/WeatherIcons.js
@@ -1,7 +1,31 @@
+
 import React from "react";
 import "./WeatherIcons.css"
 import { WeatherIcon } from 'weather-react-icons';
 
-export default function WeatherIcons() {
-  return (<div><WeatherIcon iconId={200} name="owm" night className="iconLarge" /></div>);
+export default function WeatherIcons(props) {
+  console.log(props.code);
+  const codeMapping = {
+    "01d": 32,
+    "01n": 31,
+    "02d": 34,
+    "02n": 33,
+    "03d": 26,
+    "03n": 26,
+    "04d": 28,
+    "04n": 27,
+    "09d": 35,
+    "09n": 35,
+    "10d": 11,
+    "10n": 11,
+    "11d": 3,
+    "11n": 3,
+    "13d": 14,
+    "13n": 13,
+    "50d": 20,
+    "50n": 20,
+
+  }
+  
+  return (<div><WeatherIcon iconId={codeMapping[props.code]} name="yahoo" className="iconLarge" /></div>);
 }
